212d9de196edf65f9f064c9b5851470c0fb56796
CPKreuz <cpkreu@gmail.com>
2025-02-26 23:40:25 +0100

    Refactor evaluations to decorator

--- src/web/decorator.ts
+++ src/web/decorator.ts
@@ -1,5 +1,130 @@
 import * as vscode from 'vscode';
-import { applyEvaluationsOnClb, decorator } from './evaluations.js';
+import * as mathjs from 'mathjs';
+import { formatResult } from './evaluations.js';
+
+interface EvaluatedAppend {
+    decoration: vscode.DecorationInstanceRenderOptions,
+    resultText: string
+}
+
+const decorator = vscode.window.createTextEditorDecorationType({
+	after: {
+		fontStyle: "italic"
+	},
+    dark: {
+        after: {
+            color: "#eeeeee"
+        }
+    },
+    light: {
+        after: {
+            color: "#000000"
+        }
+    }
+});
+
+function applyEvaluationsOnClb(editor: vscode.TextEditor | undefined) {
+	if (!editor || editor.document.languageId !== "calcbook")
+	{
+		return;
+	}
+
+	const evaluations = getEvaluations(editor.document);
+
+	editor.setDecorations(decorator, evaluations);
+}
+
+function getEvaluations(document: vscode.TextDocument): vscode.DecorationOptions[] {
+	const scope = { };
+	const decorations: vscode.DecorationOptions[] = [];
+
+	for (let i = 0; i < document.lineCount; i++) {
+		const line = document.lineAt(i);
+
+		if (line.isEmptyOrWhitespace || line.text.match(/^\s*#/m)) {
+			continue;
+		}
+
+		const append = getAppend(line.text, scope);
+
+		if (!append) {
+			continue;
+		}
+
+		const hoverMessage = new vscode.MarkdownString(`${append.resultText} [Copy](command:calcbook.copyToClipboard?${encodeURIComponent(append.resultText)})`);
+
+		hoverMessage.isTrusted = {
+			enabledCommands: ["calcbook.copyToClipboard"]
+		};
+
+		const decoration: vscode.DecorationOptions = {
+			range: new vscode.Range(line.range.end, line.range.end),
+			renderOptions: append.decoration,
+			hoverMessage: hoverMessage
+		};
+
+		decorations.push(decoration);
+	}
+
+	return decorations;
+}
+
+function getAppend(text: string, scope: any): EvaluatedAppend | undefined {
+	try {
+		let result;
+		const expression = mathjs.parse(text);
+
+		result = expression.compile().evaluate(scope);
+
+		const formattedResult = formatResult(result, scope);
+
+		if (expression instanceof mathjs.AssignmentNode) {
+			if (expression.value.toString() === formattedResult) {
+				return undefined;
+			}
+		}
+
+		return simpleAppend(formattedResult);
+	} catch (error) {
+		if (error instanceof Error) {
+			return errorAppend(error.message);
+		}
+	
+		return errorAppend(JSON.stringify(error));
+	}
+}
+
+function simpleAppend(text: string): EvaluatedAppend {
+	return {
+		decoration: {
+			after: {
+				contentText: ` ${text}`
+			}
+		},
+		resultText: text
+	};
+}
+
+function errorAppend(text: string): EvaluatedAppend {
+	return {
+		decoration: {
+			after: {
+				contentText: ` ${text}`
+			},
+			dark: {
+				after: {
+					color: "#eb4034"
+				}
+			},
+			light: {
+				after: {
+					color: "#a8443d"
+				}
+			},
+		},
+		resultText: text
+	};
+}
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.window.onDidChangeActiveTextEditor(applyEvaluationsOnClb);
--- src/web/evaluations.ts
+++ src/web/evaluations.ts
@@ -1,11 +1,6 @@
 import * as vscode from 'vscode';
 import * as mathjs from 'mathjs';
 
-interface EvaluatedAppend {
-    decoration: vscode.DecorationInstanceRenderOptions,
-    resultText: string
-}
-
 class ExtraContext {
 	comments: { [symbol: string]: string };
 	expressions: { [symbol: string]: string };
@@ -18,66 +13,20 @@ class ExtraContext {
 	}
 }
 
-const decorator = vscode.window.createTextEditorDecorationType({
-	after: {
-		fontStyle: "italic"
-	},
-    dark: {
-        after: {
-            color: "#eeeeee"
-        }
-    },
-    light: {
-        after: {
-            color: "#000000"
-        }
-    }
-});
-
-function applyEvaluationsOnClb(editor: vscode.TextEditor | undefined) {
-	if (!editor || editor.document.languageId !== "calcbook")
-	{
-		return;
-	}
-
-	const evaluations = getEvaluations(editor.document);
-
-	editor.setDecorations(decorator, evaluations);
-}
-
-function getEvaluations(document: vscode.TextDocument): vscode.DecorationOptions[] {
-	const scope = { };
-	const decorations: vscode.DecorationOptions[] = [];
-
-	for (let i = 0; i < document.lineCount; i++) {
-		const line = document.lineAt(i);
-
-		if (line.isEmptyOrWhitespace || line.text.match(/^\s*#/m)) {
-			continue;
+function formatResult(result: any, scope: any): string {
+	if (scope._qikDebug || scope._qikParse) {
+		if (typeof result === 'function') {
+			return result;
 		}
 
-		const append = getAppend(line.text, scope);
-
-		if (!append) {
-			continue;
-		}
-
-		const hoverMessage = new vscode.MarkdownString(`${append.resultText} [Copy](command:calcbook.copyToClipboard?${encodeURIComponent(append.resultText)})`);
-
-		hoverMessage.isTrusted = {
-			enabledCommands: ["calcbook.copyToClipboard"]
-		};
-
-		const decoration: vscode.DecorationOptions = {
-			range: new vscode.Range(line.range.end, line.range.end),
-			renderOptions: append.decoration,
-			hoverMessage: hoverMessage
-		};
-
-		decorations.push(decoration);
+		return JSON.stringify(result);
 	}
 
-	return decorations;
+	if (typeof result === 'function') {
+		return "𝑓";
+	}
+
+	return result.toString();
 }
 
 function evaluateScopeUntilLine(document: vscode.TextDocument, scope: any, line: number, context?: ExtraContext): any {
@@ -114,78 +63,4 @@ function evaluateScopeUntilLine(document: vscode.TextDocument, scope: any, line:
 	return result;
 }
 
-function getAppend(text: string, scope: any): EvaluatedAppend | undefined {
-	try {
-        let result;
-		const expression = mathjs.parse(text);
-
-		result = expression.compile().evaluate(scope);
-
-		const formattedResult = formatResult(result, scope);
-
-		if (expression instanceof mathjs.AssignmentNode) {
-			if (expression.value.toString() === formattedResult) {
-				return undefined;
-			}
-		}
-
-		return simpleAppend(formattedResult);
-    } catch (error) {
-		if (error instanceof Error) {
-			return errorAppend(error.message);
-		}
-    
-		return errorAppend(JSON.stringify(error));
-    }
-}
-
-function formatResult(result: any, scope: any): string {
-	if (scope._qikDebug || scope._qikParse) {
-		if (typeof result === 'function') {
-			return result;
-		}
-
-		return JSON.stringify(result);
-	}
-
-	if (typeof result === 'function') {
-		return "𝑓";
-	}
-
-	return result.toString();
-}
-
-function simpleAppend(text: string): EvaluatedAppend {
-	return {
-		decoration: {
-			after: {
-				contentText: ` ${text}`
-			}
-		},
-		resultText: text
-	};
-}
-
-function errorAppend(text: string): EvaluatedAppend {
-	return {
-		decoration: {
-			after: {
-				contentText: ` ${text}`
-			},
-			dark: {
-				after: {
-					color: "#eb4034"
-				}
-			},
-			light: {
-				after: {
-					color: "#a8443d"
-				}
-			},
-		},
-		resultText: text
-	};
-}
-
-export { applyEvaluationsOnClb, getEvaluations, getAppend, evaluateScopeUntilLine, formatResult, decorator, ExtraContext };
-export type { EvaluatedAppend };
+export { evaluateScopeUntilLine, formatResult, ExtraContext };
